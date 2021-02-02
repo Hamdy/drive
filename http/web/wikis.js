974fd6751ba392705c88650b53bceda63c9450c0
hamdy <hamdy.a.farag@gmail.com>
2021-02-02 17:36:56 +0200

    fixes to wikis

--- http/web/wikis.js
+++ http/web/wikis.js
@@ -8,23 +8,19 @@ var drive = require('../../drive.js')
 router.get('/drive/:id/wikis', asyncHandler(async (req, res) => {
     var driveObj = await drive.get(req.params.id)
     var filepath = "/"
-    var entry = null
-    console.log(filepath)
     try {
-        entry = await driveObj.promises.stat(filepath)
-       
+        await driveObj.promises.stat(filepath)
         var files = await driveObj.promises.readdir(filepath)
         files.sort()
         wikis = []
 
         files.map( function(file) {
-            if(file.startsWith("info_")){
+            if(["foundation", "legal","tfgrid_sdk"].includes(file)){
                 wikis.push({"name": file, "url": `/drive/${req.params.id}/wikis/${file}`})
             }
         })
         res.render('wikis/wikis.mustache', {wikis : wikis});
     } catch (e) {
-        console.log(e)
         return res.status(404).json('');
     }
 }))
@@ -47,10 +43,38 @@ router.get('/drive/:id/wikis/:wikiname', asyncHandler(async (req, res) => {
 // MD File
 router.get('/drive/:id/wikis/:wikiname/:filename', asyncHandler(async (req, res) => {
     var driveObj = await drive.get(req.params.id)
-    var filepath = `/${req.params.wikiname}/${req.params.filename}`
+    var wikiname = req.params.wikiname
     var encoding = 'utf-8'
     var filename = req.params.filename
 
+    if (filename.startsWith("file__") || filename.startsWith("page__")){
+        var splitted = filename.split("__")
+        if (splitted.length != 3){
+            return res.status(404).json('');
+        }
+        if(filename.startsWith("file__")){
+            encoding = 'binary'
+        }
+
+        filename = splitted[2]
+        wikiname = splitted[1]
+    }else if (filename.startsWith("html__")){
+		var splitted = filename.split("__")
+
+		if (splitted.length < 3){
+            return res.status(404).json('');
+        }
+
+		
+        wikiname = splitted[1]
+        splitted.shift()
+        splitted.shift()
+        filename = splitted.join("__")
+
+	}
+
+    filepath = `/${wikiname}/${filename}`
+    // `/${req.params.wikiname}/${req.params.filename}`
     var entry = null
     try {
         entry = await driveObj.promises.stat(filepath)
@@ -59,13 +83,13 @@ router.get('/drive/:id/wikis/:wikiname/:filename', asyncHandler(async (req, res)
        
     } catch (e) {
         if (filename == "README.md"){
-            filepath = `/${req.params.wikiname}/readme.md`
+            filepath = `/${wikiname}/readme.md`
             try{
                 entry = await driveObj.promises.stat(filepath)
                 var content = await  driveObj.promises.readFile(filepath, encoding);
                 return res.send(content)
             }catch(e){
-                var content =`# ${req.params.wikiname}`
+                var content =`# ${wikiname}`
                 return res.send(content)
             }            
         }
